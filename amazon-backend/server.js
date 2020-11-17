88e7146d0aa756f6cb80d079341b0cd5dccab930
gikman <72310618+gikman@users.noreply.github.com>
2020-11-17 10:54:12 +0300

    Add files via upload

---
+++ amazon-backend/server.js
@@ -0,0 +1,77 @@
+const cors = require("cors");
+const express = require("express");
+const stripe = require("stripe")("sk_test_51HjrXXCzs3DXxVFCtwkb4p8t3AVxHQtuKLQ2HQjpuOt2n0b7CDwviriCp6OsGz9Uyu1scNxt6eJBAvppKUMkDKY300AYTjK4BY");
+const uuid = require("uuid/v4");
+
+const app = express();
+const PORT = process.env.PORT || 5000; // Step 1
+
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.get("/", (req, res) => {
+  res.send("Add your Stripe Secret Key to the .require('stripe') statement!");
+});
+
+app.post("/payments/create", async (request, response) => {
+    const total = request.query.total;
+
+    console.log("Payment Request Recieved BOOM!!! for this amount >>> ", total);
+
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: total,
+        currency: "usd",
+    });
+
+    response.status(201).send({
+        clientSecret: paymentIntent.client_secret,
+    });
+});
+
+// app.post("/payments/create", async (req, res) => {
+//   console.log("Request:", req.body);
+
+//   let error;
+//   let status;
+//   try {
+//     const { basket, token } = req.body;
+
+//     const customer = await stripe.customers.create({
+//       email: token.email,
+//       source: token.id
+//     });
+
+//     const idempotency_key = uuid();
+//     const charge = await stripe.charges.create(
+//       {
+//         amount: basket.price * 100,
+//         currency: "usd",
+//         customer: customer.id,
+//         receipt_email: token.email,
+//         description: `Purchased the ${basket.title}`,
+//         shipping: {
+//           name: token.card.name,
+//           address: {
+//             line1: token.card.address_line1,
+//             line2: token.card.address_line2,
+//             city: token.card.address_city,
+//             country: token.card.address_country,
+//             postal_code: token.card.address_zip
+//           }
+//         }
+//       },
+//       {
+//         idempotency_key
+//       }
+//     );
+//     console.log("Charge:", { charge });
+//     status = "success";
+//   } catch (error) {
+//     console.error("Error:", error);
+//     status = "failure";
+//   }
+
+//   res.json({ error, status });
+// });
+
+app.listen(PORT, console.log(`Server is starting at ${PORT}`));
